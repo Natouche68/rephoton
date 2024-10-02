2eb733695165615dac8a2de2795441eb4c5d791f
Natouche68 <ngrossheny@gmail.com>
2024-10-02 14:34:30 +0200

    Add a saturation slider

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -3,6 +3,7 @@ extends Control
 
 var warm_color: Color = Color(1, 1, 1)
 var tint_color: Color = Color(1, 1, 1)
+var saturation: float = 1
 
 
 func _on_warm_slider_value_changed(value: float) -> void:
@@ -12,7 +13,7 @@ func _on_warm_slider_value_changed(value: float) -> void:
 		value *= -1
 		warm_color = Color(1 - value / 3, 1 - value / 6, 1.1)
 	
-	calculate_modulate_color()
+	calculate_colors()
 
 func _on_tint_slider_value_changed(value: float) -> void:
 	if value > 0:
@@ -21,7 +22,13 @@ func _on_tint_slider_value_changed(value: float) -> void:
 		value *= -1
 		tint_color = Color(1 - value / 4, 1.1, 1 - value / 4)
 	
-	calculate_modulate_color()
+	calculate_colors()
 
-func calculate_modulate_color():
-	$Image.modulate = (warm_color + tint_color) / 2
+func _on_saturation_slider_value_changed(value: float) -> void:
+	saturation = value
+	
+	calculate_colors()
+
+func calculate_colors():
+	$Image.material.set_shader_parameter("modulate", (warm_color + tint_color) / 2)
+	$Image.material.set_shader_parameter("saturation", saturation)
