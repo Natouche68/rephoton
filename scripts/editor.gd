ba736bb07d5e846e8a1cf70d32e02368985c179b
Natouche68 <ngrossheny@gmail.com>
2024-10-12 17:48:53 +0200

    Create the functionnality for the color adjust

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -6,19 +6,28 @@ var saturation: float = 1
 var vignette: float = 0
 
 
-func _on_warm_slider_value_changed(value: float) -> void:
-	if value > 0:
-		warm_color = Color(1.1, 1 - value / 5, 1 - value / 2)
-	elif value < 0:
-		value *= -1
-		warm_color = Color(1 - value / 3, 1 - value / 6, 1.1)
-	
-	calculate_colors()
-
-func _on_saturation_slider_value_changed(value: float) -> void:
-	saturation = value
-	
-	calculate_colors()
+func _on_color_adjust_gui_input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch or event is InputEventScreenDrag:
+		var value_x = event.position.x / $ColorAdjust.get_rect().size.x * 2 - 1
+		var value_y = event.position.y / $ColorAdjust.get_rect().size.y * 2 - 1
+		if value_x < -1:
+			value_x = -1
+		elif value_x > 1:
+			value_x = 1
+		if value_y < -1:
+			value_y = -1
+		elif value_y > 1:
+			value_y = 1
+		$ColorAdjust/Pointer.position = Vector2(value_x + 1, value_y + 1) * 64 - Vector2(8, 8)
+		
+		if value_x > 0:
+			warm_color = Color(1, 1 - value_x / 5.0, 1 - value_x / 2.0)
+		elif value_x < 0:
+			value_x *= -1
+			warm_color = Color(1 - value_x / 3.0, 1 - value_x / 6.0, 1)
+		
+		saturation = -value_y + 1
+		calculate_colors()
 
 func _on_vignette_slider_value_changed(value: float) -> void:
 	vignette = value
