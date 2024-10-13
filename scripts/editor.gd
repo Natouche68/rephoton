07757393178be015740ce80060872497dd9e81e6
Natouche68 <ngrossheny@gmail.com>
2024-10-13 18:35:18 +0200

    Add a background

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -8,8 +8,8 @@ var vignette: float = 0
 
 func _on_color_adjust_gui_input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch or event is InputEventScreenDrag:
-		var value_x = event.position.x / $ColorAdjust.get_rect().size.x * 2 - 1
-		var value_y = event.position.y / $ColorAdjust.get_rect().size.y * 2 - 1
+		var value_x = event.position.x / %ColorAdjust.get_rect().size.x * 2 - 1
+		var value_y = event.position.y / %ColorAdjust.get_rect().size.y * 2 - 1
 		if value_x < -1:
 			value_x = -1
 		elif value_x > 1:
@@ -18,7 +18,7 @@ func _on_color_adjust_gui_input(event: InputEvent) -> void:
 			value_y = -1
 		elif value_y > 1:
 			value_y = 1
-		$ColorAdjust/Pointer.position = Vector2(value_x + 1, value_y + 1) * 64 - Vector2(8, 8)
+		%ColorAdjust/Pointer.position = Vector2(value_x + 1, value_y + 1) * 64 - Vector2(8, 8)
 		
 		if value_x > 0:
 			warm_color = Color(1, 1 - value_x / 5.0, 1 - value_x / 2.0)
@@ -35,6 +35,6 @@ func _on_vignette_slider_value_changed(value: float) -> void:
 	calculate_colors()
 
 func calculate_colors():
-	$Image.material.set_shader_parameter("modulate", warm_color)
-	$Image.material.set_shader_parameter("saturation", saturation)
-	$Image.material.set_shader_parameter("vignette", vignette)
+	%Image.material.set_shader_parameter("modulate", warm_color)
+	%Image.material.set_shader_parameter("saturation", saturation)
+	%Image.material.set_shader_parameter("vignette", vignette)
